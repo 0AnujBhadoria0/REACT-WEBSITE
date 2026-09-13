7054e136125cb4e7ecff09838f88335ab5817533
User <anujbhadriya@gmail.com>
2026-09-13 13:51:06 +0530

    Describe your updates here

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="DAD">
+      <h1 id="child1">Welcome to my React Website</h1>
+      <p id="child2">This is a simple React website.</p>
+    </div>
+    <div id="CHACHU">
+      <h2 id="child3">About Me</h2>
+      <p id="child4">I am a web developer with experience in React.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
